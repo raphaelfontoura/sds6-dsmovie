5cad42420342e278ec4cad21d321fa8fc1e1dda0
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-01-11 09:19:01 -0300

    Add Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import Navbar from "./components/Navbar";
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from "components/Navbar";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
 
 function App() {
   return (
-    <Navbar />
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+const Listing = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Listing
